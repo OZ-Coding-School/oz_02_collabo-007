0b330bb51ff8cb787b44c0ab375654534055c3d2
GamjaJEON <indian_potato@naver.com>
2024-04-29 03:27:01 +0900

    feat: Label 컴포넌트 구현

--- components/core/InputLabel/InputLabel.tsx
+++ components/core/InputLabel/InputLabel.tsx
@@ -2,22 +2,34 @@ import { cn } from '@/lib/utils/cn';
 import { VariantProps, cva } from 'class-variance-authority';
 import React, { FC, InputHTMLAttributes, LabelHTMLAttributes } from 'react';
 
-export const LabelVariants = cva({
-  variants: {
-    variant: {
-      default: ' text-gray-60',
-      disabled: ' text-gray-30',
+export const LabelVariants = cva(
+  `
+    text-gray-80`,
+  {
+    variants: {
+      variant: {
+        default: '',
+        disabled: ' text-gray-30',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
     },
   },
-});
+);
 
 interface LabelProps
   extends LabelHTMLAttributes<HTMLLabelElement>,
     VariantProps<typeof LabelVariants> {
+  variant?: 'default' | 'disabled' | undefined;
   labelColor?: 'default' | 'disabled';
   label?: string;
 }
 
-export const InputLabel: FC<LabelProps> = ({ label, labelColor, ...props }) => {
-  return <label className={labelColor}>{label && label}</label>;
+export const InputLabel: FC<LabelProps> = ({ variant, label, labelColor, ...props }) => {
+  return (
+    <label className={cn(LabelVariants({ variant: labelColor }))} {...props}>
+      {label && label}
+    </label>
+  );
 };
