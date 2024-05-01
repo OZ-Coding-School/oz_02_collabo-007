a58a9d1a536caa13258c26314b68952c7a874bd2
jun_ <gurwns232@naver.com>
2024-05-01 23:55:22 +0900

    feat: 비밀번호 표시 기능

--- components/organism/signupForm/PasswordField/PasswordField.tsx
+++ components/organism/signupForm/PasswordField/PasswordField.tsx
@@ -1,24 +1,30 @@
+'use client';
+
 import InputModule from '@/components/module/InputModule/InputModule';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import visibleOffIcon from '@/app/_asset/icons/visible-off.svg';
+import visibleIcon from '@/app/_asset/icons/visible.svg';
 
 const PasswordField = () => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
   return (
     <div className="w-full relative">
       <InputModule
         label="비밀번호"
         placeholder="비밀번호 입력"
-        type="password"
+        type={passwordVisible ? 'text' : 'password'}
         inputSize="mdWith"
         name="password"
       />
       <Image
-        src={visibleOffIcon}
+        src={passwordVisible ? visibleIcon : visibleOffIcon}
         alt="visible"
         width={20}
         height={20}
         className="absolute top-[38px] right-[12px]"
+        onClick={() => setPasswordVisible((prev) => !prev)}
       />
     </div>
   );
