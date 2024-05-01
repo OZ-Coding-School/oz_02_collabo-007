192f160874965a685c2d4c31e5a1d48bcb00154e
jun_ <gurwns232@naver.com>
2024-05-02 00:22:34 +0900

    feat: 휴대폰 번호 정규식 처리('-')

--- components/organism/signupForm/PhoneField/PhoneField.tsx
+++ components/organism/signupForm/PhoneField/PhoneField.tsx
@@ -1,8 +1,19 @@
+'use client';
+
 import Button from '@/components/core/Button/Button';
 import InputModule from '@/components/module/InputModule/InputModule';
-import React from 'react';
+import React, { useState } from 'react';
 
 const PhoneField = () => {
+  const [phonData, setPhonData] = useState('');
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    const numericValue = inputValue.replace(/\D/g, '');
+
+    setPhonData(() => numericValue.replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3'));
+  };
+
   return (
     <div className="w-full flex items-end self-stretch gap-[8px]">
       <div className="flex-1">
@@ -11,6 +22,9 @@ const PhoneField = () => {
           placeholder="01012345678"
           type="text"
           name="phoneNumber"
+          value={phonData}
+          onChange={handleInput}
+          required
         />
       </div>
       <div>
