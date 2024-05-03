40f019614560c9f53c87f5f41edb68056ce53311
jun_ <gurwns232@naver.com>
2024-05-03 21:16:13 +0900

    feat: 매치 결과 카드 & 스코어 보드 module 컴포넌트 생성

---
+++ components/module/ScoreBoard/ScoreBoard.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import React, { FC } from 'react';
+import checkIcon from '@/app/_asset/icons/check-circle.svg';
+import { cn } from '@/lib/utils/cn';
+
+interface ScoreBoardProps {
+  board: {
+    users: { name: string }[];
+    scores: string[];
+  };
+  isWin?: boolean;
+}
+
+const ScoreBoard: FC<ScoreBoardProps> = ({ board, isWin = false }) => {
+  return (
+    <div className="flex items-start justify-between self-stretch">
+      <div className="flex items-center gap-[8px] text-gray-80">
+        {board.users.map(({ name }, index) => (
+          <div key={index}>{name}</div>
+        ))}
+        {isWin && <Image src={checkIcon} alt="check" width={18} height={18} />}
+      </div>
+      <div className="flex items-start gap-[4px] text-center text-headline-7">
+        {board.scores.map((score, index) => (
+          <div key={index} className={cn(`${!isWin && 'text-gray-50'}`, 'w-[18px]')}>
+            {score}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ScoreBoard;
