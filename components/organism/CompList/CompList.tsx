ef93127c78461efc0d5b04cec7df88ae22b60ffe
JeonGAMJA <indian_potato@naver.com>
2024-05-04 22:32:40 +0900

    feat: 참가 신청한 대회 페이지 마크업

--- components/organism/CompList/CompList.tsx
+++ components/organism/CompList/CompList.tsx
@@ -6,12 +6,14 @@ import CompCard from '../CompCard/CompCard';
 const CompList = ({ title, compStatus, flexDirection }: any) => {
   return (
     <div className=" flex flex-col gap-[12px] ">
-      <div className={'flex items-center justify-between'}>
-        <span className="text-headline-5 text-gray-100">{title}</span>
-        <Link href="/#" className="sub-headline-2 text-gray-60">
-          전체 목록 보기
-        </Link>
-      </div>
+      {title ? (
+        <div className={'flex items-center justify-between'}>
+          <span className="text-headline-5 text-gray-100">{title}</span>
+          <Link href="/#" className="sub-headline-2 text-gray-60">
+            전체 목록 보기
+          </Link>
+        </div>
+      ) : null}
       <div className={`flex ${flexDirection} no-scrollbar`}>
         {title === '대회 정보' ? (
           <>
