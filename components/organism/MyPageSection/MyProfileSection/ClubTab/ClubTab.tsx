a35bebf1d7dc2db74dbf64ead1b50ae6029f4c53
jun_ <gurwns232@naver.com>
2024-05-07 00:05:41 +0900

    feat: mypage 클럽, 팀 아이콘 클릭 시, 클럽,팀 정보 페이지로 이동

--- components/organism/MyPageSection/MyProfileSection/ClubTab/ClubTab.tsx
+++ components/organism/MyPageSection/MyProfileSection/ClubTab/ClubTab.tsx
@@ -1,5 +1,6 @@
 import ClubIcon from '@/app/_asset/icons/club.svg';
 import TeamIcon from '@/app/_asset/icons/team.svg';
+import Link from 'next/link';
 import React, { FC } from 'react';
 
 interface ClubTabProps {
@@ -10,20 +11,26 @@ interface ClubTabProps {
 const ClubTab: FC<ClubTabProps> = ({ club, team }) => {
   return (
     <div className="flex w-full items-start self-stretch rounded-[8px] bg-white p-[12px] shadow-card">
-      <div className="flex flex-1 flex-col items-center justify-center gap-[8px] border-r border-gray-30 pr-[8px]">
+      <Link
+        href={`club/1`}
+        className="flex flex-1 flex-col items-center justify-center gap-[8px] border-r border-gray-30 pr-[8px]"
+      >
         <div className="flex items-center justify-center gap-[4px] text-body-3 text-primary-60">
           <ClubIcon width={20} height={20} fill="#FC5214" />
           <span>클럽</span>
         </div>
         <div className="w-full text-center text-sub-headline-2">{club}</div>
-      </div>
-      <div className="flex flex-1 flex-col items-center justify-center gap-[8px] pl-[8px]">
+      </Link>
+      <Link
+        href={`team/1`}
+        className="flex flex-1 flex-col items-center justify-center gap-[8px] pl-[8px]"
+      >
         <div className="flex items-center justify-center gap-[4px] text-body-3 text-primary-60">
           <TeamIcon width={20} height={20} fill="#FC5214" />
           <span>팀</span>
         </div>
         <div className="w-full text-center text-sub-headline-2">{team}</div>
-      </div>
+      </Link>
     </div>
   );
 };
