014fa0c0276ca9f806c45af03fce62156cefb265
JeonGAMJA <indian_potato@naver.com>
2024-05-10 17:29:21 +0900

    fix: 접근 권한 버그 수정

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,15 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const refreshToken = request.cookies.get('refresh')?.value;
+  const refreshToken = request.cookies.get('refresh')?.value || null;
   const { pathname } = request.nextUrl;
-  console.log(pathname);
+
+  if (pathname === '/competition/:path+') {
+    if (refreshToken === null) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  }
 
   if (pathname === '/' || pathname === '/competition/') {
     return NextResponse.next();
@@ -18,32 +22,24 @@ export function middleware(request: NextRequest) {
     return NextResponse.next();
   }
 
-  if (!refreshToken) {
+  if (refreshToken === null) {
     return NextResponse.redirect(new URL('/', request.url));
   }
-  return NextResponse.next();
 
-  // 로그인 페이지 접근 시, 이미 로그인 되어 있다면 메인 페이지로 리다이렉트
-  // if (pathname === '/login') {
-  //   if (accessToken) {
-  //     return NextResponse.redirect(new URL('/', request.url));
-  //   }
-  //   // 로그인 페이지는 보호되지 않은 경로이므로, accessToken이 없어도 접근 가능
-  //   return NextResponse.next();
-  // }
-  // if (!accessToken) {
-  //   return NextResponse.redirect(new URL('/login', request.url));
-  // }
-  // return NextResponse.next();
+  return NextResponse.next();
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: [
+    // 인증이 필요한 경로 전체 작성
     '/',
     '/signin/',
     '/signup/',
     '/competition/',
-    // 인증이 필요한 경로 전체 작성
+    '/competition/:path+',
+    '/mypage/:path*',
+    '/club/:path*',
+    '/team/:path*',
+    '/user/:path*',
   ],
 };
