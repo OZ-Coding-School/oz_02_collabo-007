727188c5ff2e44d9079d7eeb12b53fb7459fa6b2
JeonGAMJA <indian_potato@naver.com>
2024-05-14 00:44:59 +0900

    refactor: ProfileTab 조견병경, 타입 수정

--- components/organism/MyPageSection/MyProfileSection/ProfileTab/ProfileTab.tsx
+++ components/organism/MyPageSection/MyProfileSection/ProfileTab/ProfileTab.tsx
@@ -1,35 +1,43 @@
 import Image, { StaticImageData } from 'next/image';
 import React, { FC } from 'react';
+import UserIcon from '@/app/_asset/icons/user.svg';
 
 interface ProfileTabProps {
   name: string;
   gender: string;
-  birth: string;
-  tier: string;
-  profile: StaticImageData;
+  birth: number;
+  tier: string | null;
+  imageUrl: string | null;
 }
 
-const ProfileTab: FC<ProfileTabProps> = ({ name, gender, birth, tier, profile }) => {
+const ProfileTab: FC<ProfileTabProps> = ({ name, gender, birth, tier, imageUrl }) => {
   return (
     <div className="flex w-full gap-[16px]">
-      <div className="relative h-[64px] w-[64px] rounded-full">
-        <Image
-          src={profile}
-          alt="profile"
-          fill
-          sizes="64px"
-          className="rounded-full object-cover"
-          priority
-        />
+      <div className="relative flex h-[64px] w-[64px] items-center justify-center rounded-full bg-gray-20">
+        {imageUrl ? (
+          <Image
+            src={imageUrl}
+            alt="profile"
+            fill
+            sizes="64px"
+            className="rounded-full object-cover"
+            priority
+          />
+        ) : (
+          <UserIcon width={32} height={32} fill="#787878" />
+        )}
       </div>
       <div className="flex flex-1 flex-col items-start justify-center gap-[4px]">
         <div className="text-headline-4">{name}</div>
         <div className="text- text flex items-center gap-[4px] self-stretch text-body-2 text-gray-60">
-          <div>{gender}</div>
+          <div>
+            {gender === 'male' && '남자'}
+            {gender === 'female' && '여자'}
+          </div>
           <div>{`\u00B7`}</div>
           <div>{birth}</div>
           <div>{`\u00B7`}</div>
-          <div>{tier}</div>
+          <div>{tier ? tier : '-'}</div>
         </div>
       </div>
     </div>
