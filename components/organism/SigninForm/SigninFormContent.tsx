3b72645d41918853e77473095a975dc8a1b30be8
JeonGAMJA <indian_potato@naver.com>
2024-05-16 09:47:41 +0900

    refactor: error message 조건 추가

--- components/organism/SigninForm/SigninFormContent.tsx
+++ components/organism/SigninForm/SigninFormContent.tsx
@@ -30,7 +30,7 @@ const SigninFormContent = ({
   return (
     <>
       <div className="flex flex-col gap-[20px]">
-        <div className="flex flex-col gap-[28px]">
+        <div className="relative flex flex-col gap-[28px]">
           <InputPhone
             register={register}
             errors={errors}
@@ -38,6 +38,15 @@ const SigninFormContent = ({
             setFocus={setFocus}
           />
           <InputPassword register={register} errors={errors} />
+          {errors.phone?.message || errors.password?.message ? (
+            <div className="absolute bottom-[-35px] flex items-center gap-[4px] text-body-3 text-error-60">
+              <Error className="h-[16px] w-[16px] fill-error-60" />
+              {errors.phone?.message && errors.phone?.message}
+              {!errors.phone?.message &&
+                errors.password?.message &&
+                errors.password?.message}
+            </div>
+          ) : null}
         </div>
         {/* <div className="flex justify-between">
           <div className="flex justify-center">
@@ -55,12 +64,6 @@ const SigninFormContent = ({
         </div> */}
       </div>
       {console.log(errors)}
-      {errors.phone && (
-        <div className="absolute bottom-[-24px] flex items-center gap-[4px] text-body-3 text-error-60">
-          <Error className="h-[16px] w-[16px] fill-error-60" />
-          {errors.phone.message}
-        </div>
-      )}
       <div className="flex flex-col gap-[12px]">
         <div className="h-12 w-full">
           <Button variant="primary" label="로그인" type="submit" />
