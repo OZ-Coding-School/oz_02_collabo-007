ee7d0e72990d057f4037f97d71c0fc16d89dcba3
jun_ <gurwns232@naver.com>
2024-05-17 19:58:14 +0900

    feat: middleware alert창

--- middleware.ts
+++ middleware.ts
@@ -10,7 +10,9 @@ export function middleware(request: NextRequest) {
     !refreshToken &&
     !['/', '/signin/', '/signup/', '/competition/'].includes(pathname)
   ) {
-    return NextResponse.redirect(new URL('/', request.url));
+    return NextResponse.redirect(
+      new URL('/?alert=로그인 후 이용 가능한 서비스입니다.', request.url),
+    );
   }
 
   // 로그인한 사용자가 로그인 페이지나 회원가입 페이지에 접근z하려 할 때 홈으로 리다이렉트
