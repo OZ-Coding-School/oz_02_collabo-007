907c00cea7830c47e9fedb3a28604bbbc9da1b91
JeonGAMJA <indian_potato@naver.com>
2024-05-27 00:22:14 +0900

    refactor: UserProfile 에서 UserHighlightRankingCard 분리

--- components/module/UserProfile/UserProfile.tsx
+++ components/module/UserProfile/UserProfile.tsx
@@ -5,6 +5,7 @@ import Button from '@/components/core/Button/Button';
 import { UserData } from '@/@types/user';
 import ProfileAvatar from '@/components/core/Avatar/ProfileAvatar';
 import UserInfoCard from './UserInfoCard/UserInfoCard';
+import UserHighlightRankingCard from './UserHighlightRankingCard/UserHighlightRankingCard';
 
 interface UserProfileProps {
   userData: UserData;
@@ -22,26 +23,7 @@ const UserProfile = ({ userData, loginBtn, rankingPanel }: UserProfileProps) => 
           <UserInfoCard userData={userData} loginBtn />
           <ProfileAvatar image={userData?.imageUrl} />
         </div>
-        {rankingPanel && userData ? (
-          <div className="flex w-full items-center justify-center gap-[12px] rounded-[8px] border-[1px] border-primary-60 p-[12px] shadow-md">
-            {userData.ranking ? (
-              <>
-                {/* <span className="flex gap-[4px] text-sub-headline-2 text-gray-80">
-                  {userData.ranking ? userData.ranking.single.name : null}
-                  <span>·</span>
-                  {userData.ranking ? userData.ranking.single.tier : null}
-                </span>
-                <span className="text-headline-6 text-primary-60">
-                  {userData.ranking ? userData.ranking.single.rank : null}
-                </span> */}
-              </>
-            ) : (
-              <span className="text-sub-headline-2 text-primary-60">
-                현재 등록된 대표 랭킹이 없습니다.
-              </span>
-            )}
-          </div>
-        ) : null}
+        <UserHighlightRankingCard rankingPanel={rankingPanel} userData={userData} />
         {!userData && loginBtn && (
           <Link href="/signin">
             <Button size="lg" label="로그인하러 가기" variant="primary" />
