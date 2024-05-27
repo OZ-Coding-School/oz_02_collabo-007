ec9c3284cc0b09fe6a01857915b052dff9d794e6
Hyunkyung1231 <gusrud4337@gmail.com>
2024-05-27 22:49:39 +0900

    feat: 대회 상태별 url 수정

--- components/organism/CompetitionPage/DetailPage/CompButton/CompButton.tsx
+++ components/organism/CompetitionPage/DetailPage/CompButton/CompButton.tsx
@@ -3,14 +3,35 @@ import Link from 'next/link';
 import React from 'react';
 
 const CompButton = ({ id, status }: { id: number; status: string }) => {
-  return (
-    <Link
-      href={`/competition/${id}/apply`}
-      className="flex w-full flex-col items-start pt-[20px]"
-    >
-      <Button label="대회 현황 보기" />
-    </Link>
-  );
+  switch (status) {
+    case '대회 진행전':
+      return (
+        <Link
+          href={`/competition/${id}/apply`}
+          className="flex w-full flex-col items-start pt-[20px]"
+        >
+          <Button label="대회 신청하기" />
+        </Link>
+      );
+    case '대회 진행중':
+      return (
+        <Link
+          href={`/competition/${id}/progress`}
+          className="flex w-full flex-col items-start pt-[20px]"
+        >
+          <Button label="대회 현황 보기" />
+        </Link>
+      );
+    case '대회 종료':
+      return (
+        <Link
+          href={`/competition/${id}/result`}
+          className="flex w-full flex-col items-start pt-[20px]"
+        >
+          <Button label="대회 결과 보기" />
+        </Link>
+      );
+  }
 };
 
 export default CompButton;
