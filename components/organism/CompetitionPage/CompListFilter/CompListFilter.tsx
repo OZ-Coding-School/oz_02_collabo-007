8a79944f5596f6b55e85d2c64f9d7ac153625107
JeonGAMJA <indian_potato@naver.com>
2024-05-28 20:23:41 +0900

    feat: filter 값 params에 추가 구현

--- components/organism/CompetitionPage/CompListFilter/CompListFilter.tsx
+++ components/organism/CompetitionPage/CompListFilter/CompListFilter.tsx
@@ -1,87 +1,45 @@
 'use client';
-import { useState, useRef, useEffect } from 'react';
+import React, { ChangeEvent, useCallback } from 'react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
 import DropdownIcon from '@/app/_asset/icons/dropdown.svg';
-import Link from 'next/link';
 
 interface CompListFilter {
-  filterOption: {
-    name: string;
-    options: { title: string; value: string }[];
-    defaultOption: string;
-  };
-  searchParams: { [key: string]: string };
+  filterOption: { name: string; options: { title: string; value: string }[] };
 }
 
-const CompListFilter = ({ filterOption, searchParams }: CompListFilter) => {
-  const { name, options, defaultOption } = filterOption;
+const CompListFilter = ({ filterOption }: CompListFilter) => {
+  const { name, options } = filterOption;
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
-  const [selectedValue, setSelectedValue] = useState(defaultOption);
-  const [isOpen, setIsOpen] = useState(false);
-  const dropdownRef = useRef<HTMLDivElement>(null);
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
 
-  const handleOptionClick = (title: string) => {
-    setSelectedValue(title);
-    setIsOpen(false);
+      return params.toString();
+    },
+    [searchParams],
+  );
+
+  const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    router.replace(pathname + '?' + createQueryString(name, event.target.value));
   };
-
-  const handleFilterButtonClick = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const handleOptionListClose = (e) => {
-    if (!dropdownRef.current?.contains(e.target)) {
-      setIsOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('click', handleOptionListClose);
-
-    return () => {
-      document.removeEventListener('click', handleOptionListClose);
-    };
-  }, []);
 
   return (
-    <div className="relative" ref={dropdownRef}>
-      <button
-        type="button"
-        onClick={handleFilterButtonClick}
-        className="width-full flex gap-[4px]"
-      >
-        {selectedValue}
-        <DropdownIcon
-          width={24}
-          height={24}
-          fill="#787878"
-          className={isOpen ? 'rotate-180' : 'rotate-0'}
-        />
-      </button>
-      {isOpen && (
-        <ul className="width-full absolute z-10 bg-white">
-          {options.map((option, index) => (
-            <li
-              key={index}
-              value={option.value}
-              onClick={() => handleOptionClick(option.title)}
-              className="hover:text-primary-60"
-            >
-              <Link
-                href={{
-                  pathname: `/competition`,
-                  query: {
-                    ...searchParams,
-                    [name]: option.value,
-                  },
-                }}
-                replace
-              >
-                {option.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      )}
+    <div className="flex gap-[4px]">
+      <select name={name} id={name} onChange={handleChange}>
+        {options.map((option, index) => (
+          <option key={index} value={option.value}>
+            {option.title}
+          </option>
+        ))}
+      </select>
+      <label htmlFor={name}>
+        <DropdownIcon width={24} height={24} fill="#787878" />
+      </label>
     </div>
   );
 };
