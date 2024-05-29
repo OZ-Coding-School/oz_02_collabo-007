22243fc9e2b83a937bf6dbdd8c15acdbbdb7129f
Hyunkyung1231 <gusrud4337@gmail.com>
2024-05-29 14:02:32 +0900

    refactor : handleCopyClick 코드 호환성문제로 수정

--- components/core/CopyButton/CopyButton.tsx
+++ components/core/CopyButton/CopyButton.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 import CopyIcon from '@/app/_asset/icons/textcopy.svg';
 
 const CopyButton = ({ text }: { text: string }) => {
-  const handleCopyClick = async (text: string) => {
-    try {
-      await navigator.clipboard.writeText(text);
-      alert('주소가 클립보드에 복사되었습니다.');
-    } catch (error) {
-      console.log(error);
-    }
+  const handleCopyClick = (text: string) => {
+    const textArea = document.createElement('textarea');
+    document.body.appendChild(textArea);
+    textArea.value = text;
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+    alert('주소가 클립보드에 복사되었습니다.');
   };
 
   return (
