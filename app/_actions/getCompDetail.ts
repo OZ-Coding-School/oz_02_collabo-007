92b55be0ceeeede1acf7b860d03bf9699286d09f
jun_ <gurwns232@naver.com>
2024-06-03 10:40:09 +0900

    feat: 캐싱 옵션 수정 - (1시간 & 태그)

--- app/_actions/getCompDetail.ts
+++ app/_actions/getCompDetail.ts
@@ -13,8 +13,7 @@ export const getCompDetail = async (id: number) => {
         headers: {
           Authorization: token ? `Bearer ${token.value}` : '',
         },
-        cache: 'force-cache',
-        next: { tags: [`competition-detail-${id}`] },
+        next: { revalidate: 3600, tags: [`competition-detail-${id}`] },
       },
     );
     const data = await res.json();
