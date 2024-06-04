ae5a52c9510101daa919889f5639df5583affc2b
JeonGAMJA <indian_potato@naver.com>
2024-06-04 09:15:50 +0900

    feat: CompListOptionMenuButton Circle 추가

--- components/module/CompListOptionMenuButton/CompListOptionMenuButton.tsx
+++ components/module/CompListOptionMenuButton/CompListOptionMenuButton.tsx
@@ -3,34 +3,36 @@ import { VariantProps, cva } from 'class-variance-authority';
 import Link from 'next/link';
 import React from 'react';
 
-const CompListOptionMenuButtonVariants = cva(
-  'flex items-center justify-center text-body-2',
-  {
-    variants: {
-      variant: {
-        circle: '',
-        round: `whitespace-nowrap rounded-[99px] bg-gray-20 px-[12px] py-[6px]`,
-        underBar: `h-[32px] flex-1`,
-      },
-      selected: {
-        true: '',
-        false: 'text-gray-80',
-      },
+const CompListOptionMenuButtonVariants = cva('flex items-center justify-center', {
+  variants: {
+    variant: {
+      circle: 'text-sub-headline-2 px-[16px] py-[10px] bg-gray-20 rounded-[99px]',
+      round: `whitespace-nowrap rounded-[99px] bg-gray-20 px-[12px] py-[6px] text-body-2`,
+      underBar: `h-[32px] flex-1 text-body-2`,
     },
-    compoundVariants: [
-      {
-        variant: 'round',
-        selected: true,
-        class: 'bg-primary-10 text-primary-60',
-      },
-      {
-        variant: 'underBar',
-        selected: true,
-        class: 'border-b-[2px] border-primary-60 text-primary-60',
-      },
-    ],
+    selected: {
+      true: '',
+      false: 'text-gray-80',
+    },
   },
-);
+  compoundVariants: [
+    {
+      variant: 'circle',
+      selected: true,
+      class: 'bg-primary-60 text-white',
+    },
+    {
+      variant: 'round',
+      selected: true,
+      class: 'bg-primary-10 text-primary-60',
+    },
+    {
+      variant: 'underBar',
+      selected: true,
+      class: 'border-b-[2px] border-primary-60 text-primary-60',
+    },
+  ],
+});
 
 interface CompListOptionMenuButtonProps
   extends VariantProps<typeof CompListOptionMenuButtonVariants> {
