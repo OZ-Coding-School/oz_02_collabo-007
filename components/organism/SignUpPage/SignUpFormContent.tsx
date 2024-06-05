e2d16f7911e0ee2a5444ab5e94561b678cb7e31f
JeonGAMJA <indian_potato@naver.com>
2024-06-05 16:50:26 +0900

    refactor: loadingSpinner 교체 작업

--- components/organism/SignUpPage/SignUpFormContent.tsx
+++ components/organism/SignUpPage/SignUpFormContent.tsx
@@ -16,7 +16,7 @@ import {
 import { useFormContext } from 'react-hook-form';
 import type { ClubSearchData } from '@/@types/club';
 import type { UserData } from '@/@types/user';
-import LoadingTennisBall from '@/components/core/LoadingTennisBall/LoadingTennisBall';
+import LoadingSpinner from '@/components/core/LoadingSpinner/LoadingSpinner';
 
 export interface SignUpFormContentProps {
   clubList: ClubSearchData[];
@@ -95,7 +95,7 @@ const SignUpFormContent: FC<SignUpFormContentProps> = ({
 
       {pending && (
         <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center">
-          <LoadingTennisBall />
+          <LoadingSpinner />
         </div>
       )}
     </>
