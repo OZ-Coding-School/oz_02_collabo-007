4238e3051df71c3d853dbfa3069240b49f6528dd
jun_ <gurwns232@naver.com>
2024-06-08 23:01:11 +0900

    refactor: navbar competition tab 수정

--- components/module/Navbar/Navbar.tsx
+++ components/module/Navbar/Navbar.tsx
@@ -24,7 +24,7 @@ export const NAV_LINK = [
   },
   {
     name: '대회',
-    url: '/competition/?status=진행+전',
+    url: '/competition/',
     icon: <Flag width={24} height={24} fill="#393939" />,
     fillIcon: <FlagFilled width={24} height={24} fill="#FC5214" />,
   },
