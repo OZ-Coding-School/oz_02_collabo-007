b0d54d62b5637e45007b9cffabb4b730d0e7037a
JeonGAMJA <indian_potato@naver.com>
2024-06-09 14:31:57 +0900

    refactor: compCard 버그수정, 기능 업데이트

--- components/module/CompListSection/CompList/CompCard/CompCard.tsx
+++ components/module/CompListSection/CompList/CompCard/CompCard.tsx
@@ -22,7 +22,7 @@ const CompCard = ({ comp, title, currentLocation }: CompCardProps) => {
           className="flex min-w-full flex-col gap-[16px] rounded-[8px] bg-white p-[16px] shadow-md"
         >
           <div className="flex gap-[16px]">
-            <div className="relative h-[88px] w-[88px] ">
+            <div className="relative flex h-[88px] w-[88px] flex-shrink-0 items-center justify-center rounded-[8px] bg-gray-20">
               {comp.imageUrl ? (
                 <Image
                   src={comp.imageUrl}
@@ -34,16 +34,11 @@ const CompCard = ({ comp, title, currentLocation }: CompCardProps) => {
                   priority
                 />
               ) : (
-                <div className="flex h-[88px] w-[88px] items-center justify-center rounded-[8px] bg-gray-20">
-                  <Flag width={24} height={24} fill="#787878" />
-                </div>
+                <Flag width={24} height={24} fill="#787878" />
               )}
             </div>
-            <div className="relative flex w-[199px] flex-1 flex-col gap-[4px] text-headline-6 text-gray-100">
-              <span className="absolute right-[5px] top-[-4px] text-[10px] font-thin text-gray-70">
-                대회 신청/조회시 아래 버튼을 눌러주세요
-              </span>
-              <span>{truncateText(comp.name, 10)}</span>
+            <div className="relative flex w-full flex-1 flex-col gap-[4px] overflow-hidden text-headline-6 text-gray-100">
+              <span className="w-full truncate">{comp.name}</span>
               <div className="text-gary-80 flex flex-col gap-[4px] text-body-3">
                 <span>{formatDate(comp.startDate)}</span>
                 <span>
