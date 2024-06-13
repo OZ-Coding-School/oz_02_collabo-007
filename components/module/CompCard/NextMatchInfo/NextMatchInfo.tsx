dee0416c86c81421935ab97a693fd488c6e1ac97
JeonGAMJA <indian_potato@naver.com>
2024-06-14 02:59:19 +0900

    refactor: CompCard NextMatchInfo 분기처리

--- components/module/CompCard/NextMatchInfo/NextMatchInfo.tsx
+++ components/module/CompCard/NextMatchInfo/NextMatchInfo.tsx
@@ -1,46 +1,44 @@
 import type { Matches, MyCompData } from '@/@types/competition';
 import React from 'react';
+import Clock from '@/app/_asset/icons/clock.svg';
 
-interface NextMatchInfoProps {
-  matches: Matches;
-  status?: string;
-}
+const NextMatchInfo = ({ compData }: { compData: MyCompData }) => {
+  const { matches, status, applyStatus } = compData;
 
-const NextMatchInfo = ({ compData }: MyCompData) => {
+  const beforeText =
+    (applyStatus === 'unpaid' && '참가 대기중') ||
+    (applyStatus === 'confirmed_participation' && !matches && '경기 미배정') ||
+    (matches?.courtNumber && `${matches.courtNumber}번 코트`);
+
+  const duringText = matches?.courtNumber ? `${matches.courtNumber}번 코트` : '미정';
+
   return (
     <>
       <div className="flex rounded-[8px] bg-gray-20">
-        {status === '진행 전' && !matches?.opponentUser ? (
-          <div className="flex w-full justify-center text-sub-headline-3 text-gray-60">
-            <span>아직 경기가 배정되지 않았습니다.</span>
-          </div>
-        ) : (
-          <div className="flex w-full justify-between">
-            <div className="flex gap-[4px] text-sub-headline-3 text-gray-80">
-              {matches?.myteamUser.map((player, index) => (
-                <div key={index}>
-                  <span key={index}>{player}</span>
-                  {index !== matches?.myteamUser.length - 1 && <span>·</span>}
-                </div>
-              ))}
+        <div className="flex w-full justify-between">
+          <div className="flex  gap-[4px] text-body-3 text-gray-80">
+            <span className="text-headline-8">
+              {compData.myteam.map((player: string) => player).join('\u00B7')}
+            </span>
+            {(status === 'before' && matches) || status === 'during' ? (
               <span>vs</span>
-              {matches?.opponentUser && (
-                <>
-                  {matches?.opponentUser.map((player, index) => (
-                    <div key={index}>
-                      <span key={index}>{player}</span>
-                      {index !== matches?.myteamUser.length - 1 && <span>·</span>}
-                    </div>
-                  ))}
-                </>
-              )}{' '}
-              {!matches?.opponentUser && <span className="text-gray-50">미정</span>}
-            </div>
-            <span className="text-body-3 text-gray-60">
-              {matches?.courtNumber ? <>{matches?.courtNumber}번 코트</> : <>미정</>}
+            ) : null}
+            <span className={matches?.opponentUser ? 'text-gray-80' : 'text-gray-50'}>
+              {matches?.opponentUser &&
+                matches?.opponentUser.map((player: string) => player).join('\u00B7')}
+              {status === 'before' &&
+                !matches?.opponentUser &&
+                applyStatus === 'confirmed_participation' &&
+                matches &&
+                '미정'}
+              {status === 'during' && (!matches?.opponentUser || !matches) && '미정'}
             </span>
           </div>
-        )}
+          <div className="text-body-3 text-gray-50">
+            {status === 'before' && beforeText}
+            {status === 'during' && duringText}
+          </div>
+        </div>
       </div>
     </>
   );
