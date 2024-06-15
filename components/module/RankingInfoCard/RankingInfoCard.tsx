89717fd5b8dcd2c21c6e7328c8166bff4daf7f01
JeonGAMJA <indian_potato@naver.com>
2024-06-15 18:23:16 +0900

    refactor: RankingInfoCard props 추가

--- components/module/RankingInfoCard/RankingInfoCard.tsx
+++ components/module/RankingInfoCard/RankingInfoCard.tsx
@@ -1,18 +1,29 @@
+import { UserRanking } from '@/@types/ranking';
+import Image from 'next/image';
 import React from 'react';
+import UserIcon from '@/app/_asset/icons/user.svg';
 
-const RankingInfoCard = () => {
+const RankingInfoCard = ({ userRanking }: { userRanking: UserRanking }) => {
   return (
-    <div className="flex justify-between gap-[8px] py-[12px]">
-      <span className="w-[24px]">5</span>
-      <div className="flex w-[80px] gap-[8px]">
-        <div className="h-[24px] w-[24px] rounded-[99px]  bg-gray-40"></div>
-        <span>김형섭</span>
+    <div className="flex items-center justify-between gap-[8px] py-[12px]">
+      <span className="w-[24px]">{userRanking.rank}</span>
+      <div className="jutify-center flex w-[80px] items-center gap-[8px]">
+        <div className="relative h-[24px] w-[24px] overflow-hidden rounded-full">
+          {userRanking.imageUrl == null ? (
+            <div className="flex h-full w-full items-center justify-center bg-gray-30">
+              <UserIcon className="h-[40%] w-[40%] fill-gray-60" />
+            </div>
+          ) : (
+            <Image src={userRanking.imageUrl} alt="avatar" fill sizes="24px" />
+          )}
+        </div>
+        <span>{userRanking.user.username}</span>
       </div>
       <div className="flex flex-1 justify-end">
-        <span>1,580</span>
+        <span>{userRanking.totalPoints}</span>
       </div>
       <div className="flex flex-1 justify-end">
-        <span>라온테니스</span>
+        <span>{userRanking.club ? userRanking.club.name : '무소속'}</span>
       </div>
     </div>
   );
