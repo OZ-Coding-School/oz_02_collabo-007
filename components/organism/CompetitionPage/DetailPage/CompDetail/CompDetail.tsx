e5d163b91cc4f2bc4910755da4fb4fd72f21dd0c
JeonGAMJA <indian_potato@naver.com>
2024-06-19 23:57:40 +0900

    refactor: 대회상세정보페이지 문의하기 수정

--- components/organism/CompetitionPage/DetailPage/CompDetail/CompDetail.tsx
+++ components/organism/CompetitionPage/DetailPage/CompDetail/CompDetail.tsx
@@ -4,8 +4,11 @@ import Link from 'next/link';
 import ChevronRightIcon from '@/app/_asset/icons/chevron-right.svg';
 import CompInfoCard from '@/components/module/CompInfoCard/CompInfoCard';
 import type { CompetitionDetails } from '@/@types/competition';
+import PhoneIcon from '@/app/_asset/icons/phone.svg';
+import CopyButton from '@/components/core/CopyButton/CopyButton';
 
 const CompDetail = ({ data }: { data: CompetitionDetails }) => {
+  console.log(data);
   return (
     <div className="flex w-full flex-1 flex-col gap-[24px]">
       <div className="flex w-full flex-col gap-[16px]">
@@ -29,10 +32,14 @@ const CompDetail = ({ data }: { data: CompetitionDetails }) => {
         <div className="text-headline-6">대회 상세 요강 링크</div>
         <ChevronRightIcon width={16} height={16} fill="393939" />
       </Link>
-      <Link href={'#'} className="flex w-full items-center gap-[6px]">
+      <div className="flex w-full flex-col gap-[8px]">
         <div className="text-headline-6">문의하기</div>
-        <ChevronRightIcon width={16} height={16} fill="393939" />
-      </Link>
+        <div className="flex items-center gap-[8px]">
+          <PhoneIcon width={16} height={16} fill="#393939" />
+          <span className="text-body-2 text-gray-80">{data.phone}</span>
+          <CopyButton text={data.phone} />
+        </div>
+      </div>
     </div>
   );
 };
